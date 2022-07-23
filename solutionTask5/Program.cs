7f36e19b872073e83444d5f16e7f775bee70dba8
Armen <armen_davidyants@mail.ru>
2022-07-23 16:49:45 +0300

    Четные

---
+++ solutionTask5/Program.cs
@@ -0,0 +1,27 @@
+﻿string? inputLine = Console.ReadLine();
+string? line="";
+
+
+if(inputLine != null)
+{
+  
+  int size = int.Parse(inputLine);
+  int start = 0;
+  while(start <size)
+  {
+    
+    start=start+1;
+    if(start% 2==0)
+    {
+     line=line+start+" "; 
+    
+    }
+      
+  
+  
+  
+  
+  
+  }
+Console.WriteLine(line); 
+}
