1f141573b2d5db601a0e446c46fe31ddef894f97
Armen <armen_davidyants@mail.ru>
2022-07-23 16:58:48 +0300

    Вторая цыфра

---
+++ solutionTask8/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputline = Console.ReadLine(); 
+if (inputline != null)
+{
+int inputNumber = int.Parse(inputline);
+int outputNumber = inputNumber%10;
+Console.WriteLine(outputNumber);
+
+// char[] m = inputline.ToArray();   
+//  Console.WriteLine(m[2]);  
+   
+}
