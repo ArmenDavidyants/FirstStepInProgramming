7f381545fb25daf2b4d394e33519e85890e3ab93
Armen <armen_davidyants@mail.ru>
2022-07-23 16:55:50 +0300

    от -N до Nинтервал

---
+++ solutionTask7/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputline = Console.ReadLine(); 
+if (inputline != null)
+{
+int inputNumber = int.Parse(inputline);
+int startNumber = 1;
+
+ string? outLine = " 0 ";
+ while (startNumber<inputNumber)
+  {
+     outLine = startNumber*(-1)+ outLine+ startNumber+", ";
+     startNumber++;
+  }
+//outLine=outLine+inputNumber;
+Console.WriteLine(outLine);
+
+}
