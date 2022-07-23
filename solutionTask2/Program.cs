9925a41382227d104123df98174eab323502e46f
Armen <armen_davidyants@mail.ru>
2022-07-23 16:39:33 +0300

    Третяя

---
+++ solutionTask2/Program.cs
@@ -0,0 +1,32 @@
+﻿string? firstNumber = Console.ReadLine();
+string? secondNumer = Console.ReadLine(); 
+
+if (firstNumber != null && secondNumer  != null)
+
+{
+  int maxNumber = int.Parse(firstNumber);
+  int minNumber = int.Parse(secondNumer);
+  if(maxNumber == minNumber)
+  {
+    Console.WriteLine("Равны"); 
+  }
+  else
+  {
+    if (maxNumber > minNumber)
+   {
+    
+     Console.WriteLine(maxNumber);
+     Console.WriteLine("max");
+     Console.WriteLine(minNumber);
+     Console.WriteLine("min");
+   }
+   else
+   {
+    Console.WriteLine(maxNumber);
+    Console.WriteLine("min");
+    Console.WriteLine(minNumber);
+    Console.WriteLine("max");
+  }
+
+}
+}
