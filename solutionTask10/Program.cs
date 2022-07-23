389015af75b9d2079f914dc2ec84435d2aa04ff5
Armen <armen_davidyants@mail.ru>
2022-07-23 23:01:34 +0300

    нахождение второй цифры трехзначного числа

---
+++ solutionTask10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Вариант 1 "нахождение второй цифры трехзначного числа"
+string? inputline = Console.ReadLine(); 
+int inputNumber = int.Parse(inputline);
+int numberTwo=0;
+if(inputNumber>999 || inputNumber<100)
+{
+    Console.WriteLine("ERROR");
+}
+else
+{
+numberTwo=inputNumber/10-(inputNumber/100*10);
+}
+Console.WriteLine("second number: "+numberTwo);
