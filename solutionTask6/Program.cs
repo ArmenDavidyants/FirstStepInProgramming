ec2fb708a55ea275230af77e0e40d6a75c75f98f
Armen <armen_davidyants@mail.ru>
2022-07-23 16:52:30 +0300

    Четное не четное

---
+++ solutionTask6/Program.cs
@@ -0,0 +1,14 @@
+﻿string? even = Console.ReadLine();
+if (even != null)
+{
+    int even1 = int.Parse(even);
+    if (even1 % 2==0)
+    {
+       Console.WriteLine("да");
+    }
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+
+}
