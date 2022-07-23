1e5aeec8ebf95ae41679451f20c22ce73fc8554e
Armen <armen_davidyants@mail.ru>
2022-07-23 16:43:19 +0300

    Дни недели

---
+++ solutionTask3/Program.cs
@@ -0,0 +1,44 @@
+﻿string? inputline = Console.ReadLine(); 
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+     
+     string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputNumber));
+     {
+      
+     }
+     
+     //switch(inputNumber)
+     //{
+       //case 1:
+       //Console.WriteLine("Понедельник");
+       //break;
+       //case 2:
+       //Console.WriteLine("Вторник");
+       //break;
+       //case 3:
+       //Console.WriteLine("Среда");
+       //break;
+       //case 4:
+       //Console.WriteLine("Четверг");
+       //break;
+       //case 5:
+       //Console.WriteLine("Пятница");
+       //break;
+       //case 6:
+       //Console.WriteLine("Суббота");
+       //break;
+       //case 7:
+       //Console.WriteLine("Воскресенье");
+       //break;
+    //}
+    //string[]  dayOfWeek  = new string[7];
+    //dayOfWeek[0] = "Понедельник";
+    //dayOfWeek[1] = "Вторник";
+    //dayOfWeek[2] = "Среда";
+    //dayOfWeek[3] = "Четверг";
+    //dayOfWeek[4] = "Пятница";
+    //dayOfWeek[5] = "Суббота";
+    //dayOfWeek[6] = "Воскресенье";
+    Console.WriteLine(outDayOfWeek);
+}
