a475298b26ffca1c0ee7c54d65f3ed66dd392961
Armen <armen_davidyants@mail.ru>
2022-07-23 16:36:02 +0300

    second

---
+++ solutionTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿string? inputOne = Console.ReadLine ();
+string? inputTwo = Console.ReadLine ();
+if (inputOne != null && inputTwo != null)
+{
+int inputNumberOne = int.Parse(inputOne);
+int inputNumberTwo = int.Parse(inputTwo);
+   //if (inputNumberOne == inputNumberTwo*inputNumberTwo){
+   // Console.WriteLine("yes");
+   
+  // }else {
+  //  Console.WriteLine("no");
+  // }
+    if (inputNumberTwo == Math.Sqrt(inputNumberOne))
+    {
+      Console.WriteLine ("yes");
+    }
+    else
+    {
+      Console.WriteLine ("no");
+    }
+
+
+}
+  
