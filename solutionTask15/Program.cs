34f536503deaad19a3386b941980dd1a42b8e552
Armen <armen_davidyants@mail.ru>
2022-08-02 19:13:23 +0300

    Выходные-Будние

---
+++ solutionTask15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выходные "Да" Будние "Нет"
+string? inputline = Console.ReadLine(); 
+int dayOfWeek=int.Parse(inputline);
+if (dayOfWeek==6 || dayOfWeek==7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
