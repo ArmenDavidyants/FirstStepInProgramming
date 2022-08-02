a7cec94441378d6aca27dd722ee8d82b466dfbda
Armen <armen_davidyants@mail.ru>
2022-08-02 18:57:02 +0300

    Вывод третьего элемента массива

---
+++ solutionTask13/Program.cs
@@ -0,0 +1,15 @@
+﻿//выводит третий элемент массива
+string?inputline = Console.ReadLine(); 
+char [] myArray=inputline.ToCharArray();
+
+if(myArray.Length>=3)
+{
+  Console.WriteLine(myArray[2]);
+}
+Console.WriteLine("Третьей цифры нет");
+
+
+
+
+
+
