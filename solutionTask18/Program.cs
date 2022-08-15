4db91a032db4328adf83fcb1b6d9dddfa51eb7ea
Armen <armen_davidyants@mail.ru>
2022-08-15 21:36:52 +0300

    add solution for task 18

---
+++ solutionTask18/Program.cs
@@ -0,0 +1,17 @@
+﻿void printAnswer(int number)
+{
+    if(number==1) Console.WriteLine("Допустимо значение х>0 и y>0");
+    if(number==2) Console.WriteLine("Допустимо значение х<0 и y>0");
+    if(number==3) Console.WriteLine("Допустимо значение х<0 и y<0");
+    if(number==4) Console.WriteLine("Допустимо значение х>0 и y<0");
+
+
+}
+
+string? inputline = Console.ReadLine(); 
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);
+    
+ printAnswer(inputNumber);   
+}
