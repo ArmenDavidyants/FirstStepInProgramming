cc472594720675a50451d26108b19683b5e0bb50
Armen <armen_davidyants@mail.ru>
2022-08-18 12:11:46 +0300

    Task23

---
+++ solutionTask23/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputlineN = Console.ReadLine(); 
+if (inputlineN != null)
+{
+    int numberN = int.Parse(inputlineN);
+
+    string lineN="";
+    string lineNN=string.Empty;
+    int s=1;
+    while(s<=numberN)
+    {
+        lineN=lineN+s+" ";
+        lineNN=lineNN+s*s*s+" ";
+        s++;
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
