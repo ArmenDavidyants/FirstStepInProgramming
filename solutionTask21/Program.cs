cfbb7e5c7ee4a2016c137ca64a23bc20465d664a
Armen <armen_davidyants@mail.ru>
2022-08-18 12:03:16 +0300

    task21

---
+++ solutionTask21/Program.cs
@@ -0,0 +1,31 @@
+﻿int coordXpointA;
+int coordYpointA;
+int coordXpointB;
+int coordYpointB;
+int coordZpointA;
+int coordZpointB;
+double lenghtAB;
+Console.WriteLine("Введите координаты Х точки А");
+ coordXpointA= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y точки А");
+ coordYpointA= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z точки A");
+ coordZpointA= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X точки B");
+ coordXpointB= int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите координаты Y точки B");
+ coordYpointB= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z точки B");
+ coordZpointB= int.Parse(Console.ReadLine());
+
+lenghtAB = Math.Sqrt(Math.Pow((coordXpointA-coordXpointB),2)+Math.Pow((coordYpointA-coordYpointB),2)+Math.Pow((coordZpointA-coordZpointB),2));
+
+
+
+Console.WriteLine(lenghtAB);
+
