724aad495037a0402d489aed2128e5e43c8b1ad9
Armen <armen_davidyants@mail.ru>
2022-08-18 11:34:20 +0300

    19

--- solutionTask19/Program.cs
+++ solutionTask19/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Полиндром
+int inputNumber = int.Parse(Console.ReadLine());
+int digit1=0;
+int digit2=0;
+int digit3=0;
+int digit4=0;
+int digit5=0;
+void digit()
+{
+ digit1=inputNumber/10000;
+ digit2=(inputNumber/1000)%10;
+ digit3=(inputNumber/100)%10;
+ digit4=(inputNumber/10)%10;
+ digit5=inputNumber%10;
+}
+
+
+digit();
+if(digit1==digit5&&digit2==digit4)
+{
+    Console.WriteLine("Полиндром");
+}
+else
+{
+   Console.WriteLine("Не полиндром");
+}
