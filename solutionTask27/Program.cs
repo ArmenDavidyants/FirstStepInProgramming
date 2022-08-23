028a48973a362527184dbe399a3204368a12c2be
Armen <armen_davidyants@mail.ru>
2022-08-23 22:57:35 +0300

    Task27

---
+++ solutionTask27/Program.cs
@@ -0,0 +1,15 @@
+﻿int inputNumber = int.Parse(Console.ReadLine());
+int sum =0;
+int Conculate()
+{
+  while (inputNumber>0)  
+{
+    sum=sum+inputNumber%10;
+    inputNumber=inputNumber/10;
+    
+}
+return sum;
+}
+
+Conculate();
+Console.WriteLine(sum);
