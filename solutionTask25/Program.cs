a328d483a5705246199e5e22282e5878128a417a
Armen <armen_davidyants@mail.ru>
2022-08-23 21:55:29 +0300

    Task25

---
+++ solutionTask25/Program.cs
@@ -0,0 +1,32 @@
+﻿int numberA=0;
+int numberB=0;
+int numberC=0;
+
+void Read ()
+{
+
+
+Console.Write("ВВедите число а: ");  numberA= int.Parse(Console.ReadLine());
+
+Console.Write("ВВедите число b: ");  numberB= int.Parse(Console.ReadLine()); 
+}
+
+
+ 
+ int Conculate()
+ {
+numberC=(int)Math.Pow(numberA,numberB);
+
+return numberC;
+ }
+ 
+Read();
+Conculate();
+Console.WriteLine(numberC);
+ 
+ 
+
+
+
+
+
