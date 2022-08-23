85d3069d892489de2b80a0316fef4bee7dd4cd94
Armen <armen_davidyants@mail.ru>
2022-08-23 16:36:46 +0300

    Task28

--- solutionTask28/Program.cs
+++ solutionTask28/Program.cs
@@ -1,16 +1,43 @@
-﻿Console.Write("Введите число: ");
+﻿// Console.Write("Введите число: ");
+// string? inputLine = Console.ReadLine();
+// int inputNum = int.Parse(inputLine);
+
+// Console.WriteLine(sumNums(inputNum));
+
+// int sumNums(int num)
+// {
+// int sum = 1;
+// for (int i = 1; i <= inputNum; i++)
+// {
+// //sum *= i;
+// sum=sum*i;
+// }
+// return sum;
+// }
+
+Console.Write("Введите число: ");
 string? inputLine = Console.ReadLine();
 int inputNum = int.Parse(inputLine);
 
 Console.WriteLine(sumNums(inputNum));
+Console.WriteLine(mulRec(inputNum));
 
 int sumNums(int num)
 {
 int sum = 1;
 for (int i = 1; i <= inputNum; i++)
 {
-//sum *= i;
-sum=sum*i;
+sum *= i;
 }
 return sum;
 }
+
+int mulRec(int num)
+{
+if (num == 1) {
+return 1;
+} else {
+return num * mulRec(num - 1);
+}
+}
+
