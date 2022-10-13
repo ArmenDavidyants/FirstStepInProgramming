5138d06425406a882a14e72d8059331dcc60308b
Armen <armen_davidyants@mail.ru>
2022-10-13 22:07:59 +0300

    Task42

---
+++ solutionTask39/Program.cs
@@ -0,0 +1,50 @@
+﻿
+// Метод заполнения массива случайными числами 
+int[] FillingArray(int arrayLenght)
+{
+   int[] outArray =  new int[arrayLenght];
+   int i = 0;
+    System.Random numberSintezator = new Random();
+    while(i<arrayLenght)
+    {
+        outArray[i]= numberSintezator.Next(-1000,1000);
+        i++;
+
+    }
+    return outArray;
+}
+
+//метод печатает массив
+void PrintArray(int[] inputArray)
+{
+    int i = 0;
+    while(i<inputArray.Length-1)
+    {
+        Console.Write(inputArray[i]+", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+
+int[] ReversNewArray(int[]array)
+{
+
+int lenght = array.Length;
+int[] newArray= new int[lenght];
+for(int i=0;i<lenght;i++)
+{
+
+    newArray[lenght-1-i]=array[i];
+}
+return newArray;
+}
+
+
+
+
+int[]testArray=FillingArray(12);
+PrintArray(testArray);
+int[] reversArray=ReversNewArray(testArray);
+PrintArray(reversArray);
+
---
+++ solutionTask40/Program.cs
@@ -0,0 +1,43 @@
+﻿int sideA=0;
+int sideB=0;
+int sideC=0;
+
+Readsides();
+bool res = TestTreangle(sideA,sideB,sideC);
+PrintAnswer(res);
+
+
+
+
+void Readsides ()
+{
+Console.Write("Введите число 1: ");
+ sideA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число 2: ");
+ sideB = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число 3: ");
+ sideC = int.Parse(Console.ReadLine() ?? "");
+}
+
+bool TestTreangle(int num1,int num2,int num3)
+{
+   bool answer = ((num1 + num2 > num3) && (num2 + num3 > num1) && (num1 + num3 > num2)) ? true : false; 
+return answer;
+}
+
+void PrintAnswer(bool answer)
+{
+if(answer)
+{
+  Console.WriteLine("Из этих можно собрать треугольник");
+}
+else
+{
+Console.WriteLine("Из этих нельзя собрать треугольник");
+}
+}
+
+
+
+
+
