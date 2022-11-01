a3036421cc63cb15a5928ab4f1ae8677c9bb9ec1
Armen <armen_davidyants@mail.ru>
2022-11-01 14:54:12 +0300

    Task 49,...

---
+++ solutionTask48/Program.cs
@@ -0,0 +1,99 @@
+﻿//Метод заполнения двумерного массива
+int[,] FillingArray2(int countRow, int countColumn)
+{
+   
+    System.Random numberSintezator = new Random();
+
+    int i = 0; int j = 0;
+
+    int [,] outArray = new int[countRow,countColumn];
+
+    while(i<countRow)
+    {
+        j=0;
+        while(j<countColumn)
+         {
+            outArray[i,j]=numberSintezator.Next(0,100);
+             j++;
+         }
+     i++;
+       
+    }
+     return outArray;
+
+}
+
+
+
+//Метод печати двумерного массива
+void PrintArray2(int[,] inputArray)
+{
+    int i = 0; int j=0;
+    while(i<inputArray.GetLength(0))
+    {
+        j=0;
+       while(j<inputArray.GetLength(1))
+       {
+        Console.Write(inputArray[i,j] + " ");
+        j++;
+       }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+ConsoleColor[] col =  new ConsoleColor[]
+{
+    ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,
+    ConsoleColor.DarkGray,ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+    ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,ConsoleColor.Magenta,
+    ConsoleColor.Red,ConsoleColor.White,ConsoleColor.Yellow
+};
+
+
+//Метод печати цветного двумерного массива
+void PrintColorArray2(int[,] inputArray)
+{
+    int i = 0; int j=0;
+   
+    while(i<inputArray.GetLength(0))
+    {
+        j=0;
+       while(j<inputArray.GetLength(1))
+       {
+        Console.ForegroundColor=col[new System.Random().Next(0,16)];
+        Console.Write(inputArray[i,j] + " ");
+        Console.ResetColor();
+        j++;
+       }
+        Console.WriteLine();
+        i++;
+    }
+    
+}
+
+void UpdateTwoDimArray(ref int[,] inputArray)
+{
+    int i = 0; int j=0;
+   
+    while(i<inputArray.GetLength(0))
+    {
+        j=0;
+       while(j<inputArray.GetLength(1))
+       {
+        inputArray [i,j]=i+j;
+        j++;
+       }
+        
+        i++;
+    }
+}
+
+int [,] twoDimArray=FillingArray2(5,8);
+
+
+PrintColorArray2(twoDimArray);
+Console.WriteLine();
+UpdateTwoDimArray(ref twoDimArray);
+
+PrintColorArray2(twoDimArray);
---
+++ solutionTask51/Program.cs
@@ -0,0 +1,67 @@
+﻿int[,] FillingArray2(int countRow, int countColumn)
+{
+   
+    System.Random numberSintezator = new Random();
+
+    int i = 0; int j = 0;
+
+    int [,] outArray = new int[countRow,countColumn];
+
+    while(i<countRow)
+    {
+        j=0;
+        while(j<countColumn)
+         {
+            outArray[i,j]=numberSintezator.Next(0,100);
+             j++;
+         }
+     i++;
+       
+    }
+     return outArray;
+
+}
+
+
+void PrintArray2(int[,] inputArray)
+{
+    int i = 0; int j=0;
+    while(i<inputArray.GetLength(0))
+    {
+        j=0;
+       while(j<inputArray.GetLength(1))
+       {
+        Console.Write(inputArray[i,j] + " ");
+        j++;
+       }
+        Console.WriteLine();
+        i++;
+    }
+}
+
+
+
+
+int  CalcDiagTwoDimArr( int[,] inputArray)
+{
+    int outSumDiag= 0;
+    int i = 0; 
+   
+    while(i<inputArray.GetLength(0))
+    {
+        
+       outSumDiag+=inputArray[i,i];
+       i++; 
+    
+    }
+    return outSumDiag;
+    }
+
+
+int [,] twoDimArray=FillingArray2(5,5);
+
+
+PrintArray2(twoDimArray);
+
+int sum =CalcDiagTwoDimArr(twoDimArray);
+Console.WriteLine(sum);
