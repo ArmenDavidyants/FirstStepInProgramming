8860001e3fce7140927a759042ca3e661dd7d984
Armen <armen_davidyants@mail.ru>
2022-11-07 15:56:41 +0300

    LastHomework

---
+++ solutionTask64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+		// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+		// M = 4; N = 8. -> ""4, 6, 7, 8""
+		Console.WriteLine("Введите числло M: ");
+		string? m1=Console.ReadLine();
+		Console.WriteLine("Введите число N: ");
+		string? n1=Console.ReadLine();
+        
+        int m=int.Parse(m1);
+		int n=int.Parse(n1);
+
+		void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(m, n);
+		
+		Console.WriteLine();
---
+++ solutionTask66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+		// M = 1; N = 15 -> 120
+		// M = 4; N = 8. -> 30
+		Console.WriteLine("Введите числло M: ");
+		string? m1=Console.ReadLine();
+		Console.WriteLine("Введите число N: ");
+		string? n1=Console.ReadLine();
+
+		int m=int.Parse(m1);
+		int n=int.Parse(n1);
+		
+		int CalculateSumm(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			return (end + start)*(end - start + 1)/2; //  арифметической прогрессии
+		}
+		
+		Console.WriteLine(CalculateSumm(m,n));
